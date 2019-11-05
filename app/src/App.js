720f9c8e80a9ac80171c5162b793e780ef630575
Mun <nthnam91@gmail.com>
2019-11-05 17:08:34 +0200

    Squashed commit of the following:
    commit 9934701cd945d7e9caea56ca5f5634414cb75779
    Author: João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
    Date:   Mon Nov 4 21:17:33 2019 +0200
    
        Adding match load data functions
    
    commit d754216d687334fd8bab1da46cc19ef86baa63a5
    Author: Nam Nguyen <nam.nguyen@tuni.fi>
    Date:   Sat Nov 2 21:49:40 2019 +0200
    
        add button for invite
    
    commit 0850b2cc58a568bbcb1c1c4048865647bb0415a8
    Author: Nam Nguyen <nam.nguyen@tuni.fi>
    Date:   Sat Nov 2 20:49:18 2019 +0200
    
        structure for browse match
    
    commit 9c7c74c21ec9621d42d76645ed3e86771fe78ae4
    Merge: a64da5f d675238
    Author: Nam Nguyen <nam.nguyen@viaccess-orca.com>
    Date:   Sat Nov 2 16:58:20 2019 +0200
    
        Merge branch 'US01-UserRegister' into devel
    
    commit d67523883ff22f2355507e959d624377b7c8cc10
    Author: Mun <nthnam91@gmail.com>
    Date:   Thu Oct 31 22:31:51 2019 +0200
    
        place holder page for message list
    
    commit b63eba8253351d900de4f1070416baec79182a37
    Author: Mun <nthnam91@gmail.com>
    Date:   Wed Oct 30 07:59:07 2019 +0200
    
        #6 new city picker
    
    commit bc817a699f3c123c13b95307357456fe64296043
    Author: Nam Nguyen <nam.nguyen@viaccess-orca.com>
    Date:   Tue Oct 22 21:02:09 2019 +0300
    
        #6 connect to api create user
    
    commit 13fd1e2b00ba3df1b592ad04c16cbc2db0fa531d
    Author: Mun <nthnam91@gmail.com>
    Date:   Tue Oct 22 09:33:47 2019 +0300
    
        #6 add pick language boxes in profile page
    
    commit 6621310de979e2a31ebfa63cdf3fc15daec3aa7f
    Author: Mun <nthnam91@gmail.com>
    Date:   Mon Oct 21 22:34:32 2019 +0300
    
        Squashed commit of the following:
    
        commit 0b21936645c7b071c5bc70cb908390cfd3d8a07c
        Author: João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
        Date:   Mon Oct 21 18:40:36 2019 +0300
    
            #6 - Adding user profile picture settings
    
        commit ec2f385b33ff531ce2f2222ed303267c168143f1
        Author: João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
        Date:   Tue Oct 15 23:37:51 2019 +0300
    
            #6 - Adding user model settings and mongoose configuration
    
    commit faa6b8db4a44d26db8de628196e917e355ab1af1
    Author: Mun <nthnam91@gmail.com>
    Date:   Mon Oct 21 07:44:32 2019 +0300
    
        Language preference page
    
    commit 867485c5cf34f0bcb1f306ce3ad7ccda9c94c2aa
    Author: Mun <nthnam91@gmail.com>
    Date:   Mon Oct 14 00:04:30 2019 +0300
    
        Initial front end structure
    
    commit a64da5f24697e18cc400c1c6d7a750d700a94028
    Author: João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
    Date:   Tue Oct 8 23:23:47 2019 +0300
    
        #3 - Editing files structure
    
    commit b21c7810abe0efdf3f068205ff8167be59f7e421
    Author: João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
    Date:   Tue Oct 8 22:12:19 2019 +0300
    
        #3 - Removing non necessary files
    
    commit 584e16c75be11283de8212b68a65bd8774f458e8
    Author: João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
    Date:   Tue Oct 8 22:09:14 2019 +0300
    
        #3 - Adding backend and frontend of application

---
+++ app/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import {Colors} from './components/constant/index'
+
+import './App.css';
+import LoginPage from './containers/LoginPage'
+import EditProfilePage from './containers/EditProfilePage'
+import PreferencePage from './containers/PreferencePage'
+import PartnerListPage from './containers/PartnerListPage'
+import BrowseMatch from './containers/BrowseMatch'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+        main: Colors.mainColor
+      }
+    }
+  },
+)
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+            <LoginPage />
+        </Route>
+        <Route exact path="/edit-profile">
+            <EditProfilePage />
+        </Route>
+        <Route exact path="/partner-list">
+            <PartnerListPage />
+        </Route>
+        <Route exact path="/browse-match">
+            <BrowseMatch />
+        </Route>
+      </Switch>
+    </Router>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
---
+++ app/src/containers/BrowseMatch/index.js
@@ -0,0 +1,385 @@
+
+import React from 'react';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	Material UI
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import {withStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	Styles
+
+import ResponsiveDrawer from '../MenuDrawer';
+
+
+
+const styles = ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+    },
+    gridList: {
+        flexWrap: 'nowrap',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+    },
+    fullWidth: {
+        width: "100%",
+    },
+    bottomMargin: {
+        marginBottom: '2.5em',
+    },
+    title: {
+        color: '#fff',
+    },
+    titleBar: {
+        background:
+            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+    preferencesLink: {
+        color: '#3f51b5'
+    },
+    cardContent: {
+        padding: '0'
+    },
+    gridListTile: {
+        // height: "100% !important",
+        maxWidth: "300px",
+        minWidth: 60
+    },
+    gridListTileBar: {
+        background: "#3f51b5",
+    },
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	Class
+
+class BrowseMatch extends React.Component {
+
+
+    state = {
+        userMatches: [
+        {
+            languageName: "English",
+            matches:
+            [
+                {user:{_id: "1", name:"Nam"}},
+                {user:{name:"Peter"}},
+                {user:{name:"Jp"}},
+                {user:{name:"Nam"}},
+                {user:{name:"Peter"}},
+                {user:{name:"Jp"}},
+                {user:{name:"Nam"}},
+                {user:{name:"Peter"}},
+                {user:{name:"Jp"}},
+                {user:{name:"Nam"}},
+                {user:{name:"Peter"}},
+                {user:{name:"Jp"}}
+            ]
+        },
+        {
+            languageName: "Finnish",
+            matches:
+            [
+                {user:{name:"Nam"}},
+                {user:{name:"Peter"}},
+                {user:{name:"Jp"}}
+            ]
+        }
+    ],
+        open: false,
+    };
+    // =========================================== FUNCTIONS ======================================
+    // Load possible matches list
+    loadDataPossibleUserMatches(){
+        // http://localhost:3000/api/v1/usersMatch/possibleMatchs
+        const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/api/v1/usersMatch/possibleMatchs")
+        console.log(url)
+        fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+       
+    }
+
+    // Send a new match invitation
+    requestNewUserMatch(){
+        // http://localhost:3000/api/v1/usersMatch/sendRequest
+        // The requester user ID will be collect automatic by the server
+        const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/api/v1/usersMatch/sendRequest")
+        console.log(url)
+        fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            recipientUserID: this.state.recipientUserID,//User that will receive the request
+        })
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }
+
+    // Get all the match requests made to the user by other users
+    getMatchRequestsReceivedByTheUser(){
+        // http://localhost:3000/api/v1/usersMatch/receiptMatchsRequests
+        const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/api/v1/usersMatch/receiptMatchsRequests")
+        console.log(url)
+        fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+       
+    }
+
+    // Get the match requests made by the user to other users
+    getMatchRequestsRequestedByTheUser(){
+        // http://localhost:3000/api/v1/usersMatch/requestedMatchsRequests
+        const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/api/v1/usersMatch/requestedMatchsRequests")
+        console.log(url)
+        fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+       
+    }
+
+    // Get the current matches of the user
+    getUserCurrentMatchs(){
+        // http://localhost:3000/api/v1/usersMatch/getUserActiveMatches
+        const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/api/v1/usersMatch/getUserActiveMatches")
+        console.log(url)
+        fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+        
+    }
+
+    acceptNewMatchRequest(){
+        // need the match ID
+        const matchID = 123456;
+
+        // http://localhost:3000/api/v1/usersMatch/acceptMatchRequest/:matchid
+        const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/api/v1/usersMatch/acceptMatchRequest"+matchID);
+        
+        console.log(url)
+        fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+        
+    }
+
+    denyNewMatchRequest(){
+        // need the match ID
+        const matchID = 123456;
+
+        // http://localhost:3000/api/v1/usersMatch/denyMatchRequest/:matchid
+        const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/api/v1/usersMatch/denyMatchRequest"+matchID);
+        
+        console.log(url)
+        fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+        
+    }
+
+// ================================================================================================
+
+    componentWillReceiveProps(nextProps) {
+        const matches = nextProps.languageMatches;
+        this.setState(
+            {
+                userMatches: matches
+            }
+        );
+    }
+
+    openModal = () => {
+        this.setState({open: true})
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
+    getMatchesTiles(item, classes) {
+        return (
+            item.matches.length === 0 ? (
+                <Typography variant="h5" gutterBottom>
+                    <Typography variant="overline" gutterBottom>
+                        {"No_matches_found_for"} {item.languageName}
+                    </Typography>
+                    <Link href="/languagePreferences" className={classes.preferencesLink}>
+                        {"Edit_your_preferences"}
+                    </Link>
+                </Typography>
+            ) : (
+                <div className={classes.fullWidth}>
+                    <GridList className={classes.gridList} cols={4}>
+                        {
+                            item.matches.map((match, key) =>
+                                <GridListTile key={key}
+                                              className={classes.gridListTile}>
+                                   <Typography variant="overline" gutterBottom>
+                                   {match.user.name}
+                    </Typography>  
+                    <GridListTileBar
+            //   title={"aaa"}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <Button variant="contained" color = "primary" onClick = {this.onInviteAction.bind(this, match)}>
+                    Invite 
+                </Button>
+              }
+            />
+            </GridListTile>
+            )
+                        }
+                    </GridList>
+                </div>
+            )
+        )
+    }
+
+    getAlreadyExistsDiv(item, classes) {
+        return (<ListItem key={item.languageName}
+                          className={classes.fullWidth + ' ' + classes.bottomMargin}>
+            <Typography variant="h5" gutterBottom>
+                <Typography variant="overline" gutterBottom>
+                    {"Existing_match_found_for"} {item.languageName}
+                </Typography>
+                <Link href="/listMatches" className={classes.preferencesLink}>
+                    {"See_your_matches"}
+                </Link>
+            </Typography>
+        </ListItem>)
+    }
+
+    getMatchesList(item, classes) {
+        return (<ListItem
+            key={item.languageName}
+            className={classes.fullWidth + ' ' + classes.bottomMargin}>
+            <div className={classes.fullWidth} key={item.languageName}>
+                <div className={classes.fullWidth}>
+                    <ListItemText className={classes.bottomMargin}>
+                        <Typography variant="overline" gutterBottom>
+                            {"Can_teach" + ' ' + item.languageName + ":"}
+                        </Typography>
+                        
+                    </ListItemText>
+                </div>
+                {
+                    this.getMatchesTiles(item, classes)
+                }
+            </div>
+        </ListItem>);
+    }
+
+
+    onInviteAction (user) {
+        console.log(user)
+    }
+
+
+    render() {
+        const {classes} = this.props;
+
+        return (
+            
+            <div className={classes.root}>
+                <ResponsiveDrawer title = "Find a new language partner">
+                <List component="nav" className={classes.fullWidth}>
+                    {
+                        this.state.userMatches.map(item => {
+                            return item.alreadyExists ? (
+                                this.getAlreadyExistsDiv(item, classes)
+                            ) : (
+                                this.getMatchesList(item, classes)
+                            )
+                        })
+                    }
+                </List>
+                </ResponsiveDrawer>
+            </div>
+        );
+    }
+}
+
+BrowseMatch.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(BrowseMatch);
