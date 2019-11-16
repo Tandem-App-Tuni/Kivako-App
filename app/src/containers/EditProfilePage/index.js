2baddd3fd7a2fc3f51984b395841287347362955
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2019-11-16 15:03:21 +0200

    testing changes

--- app/src/containers/EditProfilePage/index.js
+++ app/src/containers/EditProfilePage/index.js
@@ -272,30 +272,27 @@ this.setState(
  )
 };
 
-checkLogin = () =>{
+checkUserIsRegistered = () =>{
   const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/api/v1/users/isRegistered")
-  console.log(url)
+  console.log('Checking is the user is registered...');
+  console.log(url);
+
   fetch(url, {
-  method: 'GET',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  credentials: 'same-origin'
-}).then((response) => response.json())
-.then((responseJson) => {
-  console.log(responseJson);
-})
-.catch((error) => {
-  console.error(error);
+  method: 'GET'
+  //credentials: 'same-origin'
+  }).then((response) => response.json())
+  .then((responseJson) => {
+    console.log("log");
+    console.log(responseJson);
+  })
+  .catch((error) => {
+    console.error(error);
 });
 
-
-//this.uploadPhoto("5daf39de47435bd5d59687c6");
 }
 
 componentDidMount(){
-  this.checkLogin();
+  this.checkUserIsRegistered();
 
 }
 
--- app/src/containers/LoginPage/index.js
+++ app/src/containers/LoginPage/index.js
@@ -8,15 +8,17 @@ import React, {Component} from 'react';
 
 class LandingPage extends Component {
 
-  onLoginButtonClicked = () =>{
-      const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/login")
-      console.log(url)
+  /*onLoginButtonClicked = () =>{
+      //const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/login");
+      const url = 'http://localhost:3000/login';
+      console.log(url);
       fetch(url, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      }
+        method: 'GET',
+        mode:'no-cors',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
     }).then((response) => response.json())
     .then((responseJson) => {
       console.log(responseJson);
@@ -24,7 +26,28 @@ class LandingPage extends Component {
     .catch((error) => {
       console.error(error);
     });
-  }
+  };*/
+
+  onLoginButtonClicked = () =>{
+    const url = new URL(window.location.protocol + '//' + window.location.hostname + ":3000/login");
+    //const url = 'http://localhost:3000/login';
+    window.open(url);
+    /*
+    console.log(url);
+    fetch(url, {
+      method: 'POST',
+      mode:'no-cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then((responseJson) => {
+      console.log(responseJson);
+    })
+    .catch((error) => {
+      console.error(error);
+    });*/
+  };
 
   
 
