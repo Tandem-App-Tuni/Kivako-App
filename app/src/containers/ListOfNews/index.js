8806c687631bab84c46bf09447c4c269af8764ed
Tamara Islam Meghla <tamara.meghla@tuni.fi>
2020-10-22 12:06:51 +0600

    axios is uninstalled and ListOfNews/index.js is updated

--- app/src/containers/ListOfNews/index.js
+++ app/src/containers/ListOfNews/index.js
@@ -12,7 +12,7 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import axios from 'axios';
+
 
 const styles = ({
     root: {
