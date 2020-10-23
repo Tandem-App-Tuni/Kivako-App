ca9fd4d073e967d70b6b74bc375e488e61459ab2
Tamara Islam Meghla <tamara.meghla@tuni.fi>
2020-10-24 01:47:31 +0600

    util.js has been removed

--- app/src/containers/ListOfNews/util.js
+++
@@ -1,7 +0,0 @@
-exports.compareDate = void 0;
-
-var compareDate = function compareDate(firstDate, secondDate) {
-  return new Date(secondDate) - new Date(firstDate);
-};
-
-exports.compareDate = compareDate;
