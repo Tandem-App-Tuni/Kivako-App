3641b921a06ac33ef8276c7fb112673eafeca2ea
Janaka Nawagamuwa <janaka.nawagamuwa@tuni.fi>
2021-03-22 01:23:12 +0530

    Adding configs

--- src/config_constants.js
+++ src/config_constants.js
@@ -1,8 +1,11 @@
 const LOCAL_TEST_ENVIRONMENT = process.env.LOCAL_TEST_ENVIRONMENT || false;
+console.log('LOCAL_TEST_ENVIRONMENT --- ', LOCAL_TEST_ENVIRONMENT)
 
 // set the test server url and port here; either remote hosted staging server or local server
 const testServerUrl = process.env.BACKEND_URL || 'http://localhost:3000';
 const testServerPort = process.env.BACKEND_PORT || ':3000';
+
+console.log('testServerUrl, testServerPort ', testServerUrl, testServerPort )
 
 module.exports = Object.freeze({
     APPLICATION_URL: LOCAL_TEST_ENVIRONMENT ? testServerUrl : 'https://www.unitandem.fi',
