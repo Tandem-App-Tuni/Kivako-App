ee27473184249d2d143f1477f1df573f25be338c
Elisa <elisa.akkanen@tuni.fi>
2021-03-31 19:38:09 +0300

    SignUp page bugfix for to-learn list

--- src/containers/RegisterPage/index.js
+++ src/containers/RegisterPage/index.js
@@ -366,9 +366,14 @@ class SignUpPage extends Component {
 
     this.state.languagesToTeach.forEach(item => {
       langs.push(item.language);
-    }
-    )
-
+    })
+    /*
+    this.state.languagesToLearn.forEach(item => {
+      langs.push(item.language);
+    })
+    */
+    console.log("Excluded languages list: ")
+    console.log(langs)
     return langs
   }
 
