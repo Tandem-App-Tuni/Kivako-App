ed1fb22099a3d0a326f88d54cfdb0d8223b5400c
Janaka Nawagamuwa <janaka.nawagamuwa@tuni.fi>
2021-04-05 13:35:21 +0530

    adding config varialble chanes

--- src/config_constants.js
+++ src/config_constants.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const LOCAL_TEST_ENVIRONMENT = process.env.REACT_APP_LOCAL_TEST_ENVIRONMENT || true;
+const LOCAL_TEST_ENVIRONMENT = process.env.REACT_APP_LOCAL_TEST_ENVIRONMENT || false;
 
 // set the test server url and port here; either remote hosted staging server or local server
 const testServerUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
@@ -9,7 +9,7 @@ const testServerPort = process.env.REACT_APP_BACKEND_PORT || ':3000';
 console.log('testServerUrl, testServerPort logs ', testServerUrl, testServerPort )
 
 console.log('LOCAL_TEST_ENVIRONMENT --- ', process.env.REACT_APP_LOCAL_TEST_ENVIRONMENT)
-console.log('BACKEND_URL --- ', process.env.BACKEND_URL)
+console.log('BACKEND_URL --- ', process.env.REACT_APP_BACKEND_URL)
 console.log('NODE_ENV --- ', process.env.REACT_APP_NODE_ENV)
 
 module.exports = Object.freeze({
