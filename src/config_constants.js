f07284c630fd3546dbe4e64af8c8a1c21f674216
Janaka Nawagamuwa <janaka.nawagamuwa@tuni.fi>
2021-04-10 23:33:33 +0530

    Hardcoding BE URL

--- src/config_constants.js
+++ src/config_constants.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const LOCAL_TEST_ENVIRONMENT = process.env.REACT_APP_LOCAL_TEST_ENVIRONMENT || false;
 
+process.env.REACT_APP_BACKEND_URL = 'https://unitandem-be.herokuapp.com'
+
 // set the test server url and port here; either remote hosted staging server or local server
 const testServerUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
 const testServerPort = process.env.REACT_APP_BACKEND_PORT || ':3000';
