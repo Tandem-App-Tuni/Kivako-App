a9dfa4aff29dcf31d690116185c86f6d61f91613
Janaka Nawagamuwa <janaka.nawagamuwa@tuni.fi>
2021-04-10 23:55:47 +0530

    hard coading. the test server URL

--- src/config_constants.js
+++ src/config_constants.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 const LOCAL_TEST_ENVIRONMENT = process.env.REACT_APP_LOCAL_TEST_ENVIRONMENT || false;
 
-process.env.REACT_APP_BACKEND_URL = 'https://unitandem-be.herokuapp.com'
+// process.env.REACT_APP_BACKEND_URL = 'https://unitandem-be.herokuapp.com';
 
 // set the test server url and port here; either remote hosted staging server or local server
-const testServerUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
+// const testServerUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
+const testServerUrl = 'https://unitandem-be.herokuapp.com';
 const testServerPort = process.env.REACT_APP_BACKEND_PORT || ':3000';
 
 console.log('testServerUrl, testServerPort logs ', testServerUrl, testServerPort )
