df7024682659cc79d61de30a3f5075ee608c6856
Janaka Nawagamuwa <janaka.nawagamuwa@tuni.fi>
2021-04-11 22:02:47 +0530

    Adding proxy to BE

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+import ConstantsList from '../../config_constants';
+
 const express = require ('express')
 const app = express();
 const http = require('http').Server(app);
 const path = require( 'path')
+
 let port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -10,6 +13,15 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build') + '/index.html');
 });
+
+// Proxy endpoints
+app.use('/service', createProxyMiddleware({
+  target: ConstantsList.APPLICATION_URL,
+  changeOrigin: true,
+  pathRewrite: {
+      [`^/service`]: '',
+  },
+}));
 
 app.listen(port, () => {
   console.log(`Kivako app running on port ${port}`);
