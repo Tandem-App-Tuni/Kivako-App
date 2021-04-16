c65feba246ef942d28098bf22939c9636ab1ba43
Renata Papp <renata.papp@tuni.fi>
2021-04-16 17:28:13 +0300

    v1 of accessibility statement

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -17,6 +17,10 @@ import unitandem_logo from "./images/unitandem_logo.png"
 import cover_pic from "./images/cover_photo.png"
 import ListOfNews from '../ListOfNews'
 
+// ------------------- material ui stuff for accessibility statement dialog
+import { Dialog, DialogTitle, DialogContent, DialogActions, Typography } from '@material-ui/core';
+import ButtonMUI from '@material-ui/core/Button'
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
 const useStyles = theme =>
   ({
@@ -45,7 +49,9 @@ class LandingPage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = ({ redirectToSingUp: false, redirectToLogin: false });
+    this.state = ({ redirectToSingUp: false, 
+                    redirectToLogin: false, 
+                    openAccessibilityStatement: false });
   }
 
   onLoginButtonClicked() {
@@ -56,13 +62,21 @@ class LandingPage extends Component {
     this.setState({ redirectToSingUp: true });
   }
 
+  onAccessibilityStatementButtonClicked() {
+    this.setState({ openAccessibilityStatement: true });
+  }
+
+  onCloseAccessibilityStatement() {
+    this.setState({ openAccessibilityStatement: false })
+  }
+
   render() {
 
     if (this.state.redirectToSingUp) return (<Redirect to='/register' />);
     if (this.state.redirectToLogin) return (<Redirect to='/local-login' />);
 
     return (
-
+      <div>
       <div className="container">
         <nav className="navbar navbar-expand-md navbar-light">{/* navigation starts here */}
           <a href="."><img className="nav-logo" src={unitandem_logo} alt="unitandem logo" /></a>
@@ -74,6 +88,7 @@ class LandingPage extends Component {
             <ul className="navbar-nav ml-auto navbar-right">
               <a className="nav-text" href=".">Home</a>
               <a className="nav-text" href="#footer-section">Contact</a>
+              <a className="nav-text" onClick={() => {this.onAccessibilityStatementButtonClicked()}}>Accessibility Statement</a>
             </ul>
           </div>
         </nav> {/* navigation ends here */}
@@ -254,7 +269,40 @@ class LandingPage extends Component {
               <img className="footer-logo3" src={cc_image} alt="cc logo" />
             </div>
           </div>
-        </section>   {/* footer-section ends here  */}       </div >
+        </section>   {/* footer-section ends here  */}
+      </div >
+
+      
+        <Dialog open={this.state.openAccessibilityStatement} onClose={() => {this.onCloseAccessibilityStatement()}}>
+          <DialogTitle disableTypography={true}><Typography variant="h4" component="h1">Accessibility Statement for UniTandem</Typography></DialogTitle>
+          <DialogContent>
+              <Typography variant="body1" gutterBottom>Tampere University of Applied Sciences is committed to ensuring digital accessibility for people with disabilities. We are continually improving the user experience for everyone, and applying the relevant accessibility standards.</Typography>
+              <Typography variant="h5" component="h2" gutterBottom>Conformance status</Typography>
+              <Typography variant="body1" gutterBottom>The <a href="https://www.w3.org/WAI/standards-guidelines/wcag/">Web Content Accessibility Guidelines (WCAG)</a> defines requirements for designers and developers to improve accessibility for people with disabilities. It defines three levels of conformance: Level A, Level AA, and Level AAA. UniTandem is not assessed with WCAG 2.1 level AA. Not assessed means that the content has not been evaluated or the evaluation results are not available.</Typography>
+              <Typography variant="h5" component="h2" gutterBottom>Feedback</Typography>
+              <Typography variant="body1" gutterBottom>We welcome your feedback on the accessibility of UniTandem. Please let us know if you encounter accessibility barriers on UniTandem: </Typography>
+              <Typography> 
+                <ul>
+                  <li>Phone: <a href="tel: +358 294 5222">+358 294 5222</a></li>
+                  <li>E-mail: <a href="mailto: info@unitandem.fi"> info@unitandem.fi</a></li>
+                  <li>Postal Address: Kuntokatu 3, 33520 Tampere, Finland</li>
+                </ul>
+              </Typography>
+              <Typography variant="body1" gutterBottom> We try to respond to feedback within 5 business days. </Typography>
+              <Typography variant="h5" component="h2" gutterBottom>Assessment approach</Typography>
+              <Typography variant="body1" gutterBottom>Tampere University of Applied Sciences assessed the accessibility of UniTandem by the following approaches: </Typography>
+              <Typography>
+                <ul>
+                  <li>Self-evaluation: the content was evaluated by our own organization</li>
+                </ul>
+              </Typography>
+
+          </DialogContent>
+          <DialogActions>
+            <ButtonMUI style={{margin: "1% 5% 2% 5%"}} variant="contained" onClick={() => {this.onCloseAccessibilityStatement()}}>Close</ButtonMUI>
+          </DialogActions>
+        </Dialog>
+      </div>
 
     )
 
